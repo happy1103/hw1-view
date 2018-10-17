eae2716477d4f95a925d7df981c841dbe148ba52
User10 <user10@user10de-imac.local>
2018-10-17 12:17:51 +0800

    Initial Commit

---
+++ hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by User10 on 2018/10/17.
+//  Copyright © 2018 ee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
